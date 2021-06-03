b97d317c545f1ec37c953b925129302f7a32e4db
scan3ls <scan3ls@gmail.com>
2021-06-03 17:22:41 -0500

    Add inital file to test testing

---
+++ tests/testing.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+
+describe("Test suite", () => {
+    it('True is True', () => { assert.equal(true, true) });
+    it('False is False', () => { assert.equal(false, false) });
+});
