1cb02488fcbecd227d5c7fb694bd3ecedd1088ac
scan3ls <scan3ls@gmail.com>
2021-06-04 12:02:12 -0500

    Add Basic tests for utils/redis.js

--- tests/testing.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-
-describe("Test suite", () => {
-    it('True is True', () => { assert.equal(true, true) });
-    it('False is False', () => { assert.equal(false, false) });
-});
