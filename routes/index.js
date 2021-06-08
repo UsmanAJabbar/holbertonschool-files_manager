b3429468069f5978767c398e2b65e69b92abcd6d
scan3ls <scan3ls@gmail.com>
2021-06-08 15:10:28 -0500

    Update

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,6 @@ router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.post('/files', FilesController.postUpload);
+// router.post('/files', FilesController.postUpload);
 
 module.exports = router;
