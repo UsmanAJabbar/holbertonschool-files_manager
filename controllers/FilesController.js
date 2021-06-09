870e59f92c3b653b88adc60b86896c86f988cc0f
UsmanGTA <hi@usmanjabbar.com>
2021-06-09 18:57:57 +0000

    Added initial commit for task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -17,7 +17,6 @@ class FilesController {
       let collection = database.collection('users')
       const user = await collection.findOne({ _id: new mongo.ObjectID(mongoUserId) });
 
-
       const { name, type, parentId = 0, isPublic = false, data } = req.body;
       const fileTypeOptions = ['folder', 'file' , 'image'];
 
@@ -38,23 +37,82 @@ class FilesController {
         type,
         isPublic,
         parentId,
-        localPath: `${path}/${uuid4()}`,
       };
-      
-      console.log(file);
-      res.status(201).json(file);
 
-      // collection = database.collection('files');
-      // collection.insert(file, (err, file) => {
-      //   res.status(201).json({
-      //     id: file._id,
-      //     userId: file.userId,
-      //     name: file.name,
-      //     type: file.type,
-      //     isPublic: file.isPublic,
-      //     parentId: file.parentId,
-      //   });
-      // });
+      if (['file', 'image'].includes(file.type)) {
+        if (file.parentId !== 0){
+          path = `${path}/${file.parentId}`;
+        }
+        file.localPath = `${path}/${uuid4()}`;
+      }
+      collection = database.collection('files');
+      collection.insertOne(file, (err, result) => {
+        if (err) return;
+        const file = result.ops[0];
+
+        const decode = (base64) => {
+          const buffer = Buffer.from(base64, 'base64');
+          return buffer.toString('utf-8');
+        };
+
+        fs.mkdir(path, () => {
+          fs.writeFile(file.localPath, decode(data), (err) => {
+            if (err) res.status(500).send(`oh no\n${err.message}`);
+            else res.status(201).json({
+              id: file._id,
+              userId: file.userId,
+              name: file.name,
+              type: file.type,
+              isPublic: file.isPublic,
+              parentId: file.parentId,
+            });
+          });
+        });
+
+      });
+    }
+  }
+  static async getShow (req, res) {
+    const fileId = req.body.id;
+    const token = req.headers['x-token'];
+
+    if (!token || !fileId) res.status(401).json({ error:'Unauthorized' });
+    else {
+      const database = await DBClient.connection;
+      const userColl = database.collection('users');
+      const fileColl = database.collection('files');
+
+      const mongoUserId = await RedisClient.get(`auth_${token}`);
+      const user = await userColl.findOne({ _id: new mongo.ObjectId(mongoUserId) });
+      if (user) {
+        const file = await fileColl.findOne({ _id: new mongo.ObjectId(fileId), userId: user._id.toString() })
+        if (!file) res.status(404).json({ error:'Not found' });
+        else res.status(200).json(file);
+      }
+    }
+  }
+  static async getIndex (req, res) {
+    const token = req.headers['x-token'];
+    const parentId = req.query.parentId || 0;
+    const page = req.query.page || 0;
+
+    const database = await DBClient.connection;
+    const userColl = database.collection('users');
+    const fileColl = database.collection('files');
+
+    const mongoUserId = await RedisClient.get(`auth_${token}`);
+    const user = await userColl.findOne({ _id: new mongo.ObjectId(mongoUserId) });
+
+    if (!user) res.status(401).json({ error:'Unauthorized' });
+    else {
+      const docsPerPage = 20;
+      const filterQuery = [ 
+                            { '$match': { parentId, userId: user._id.toString() } },
+                            { '$limit': docsPerPage },
+                            { '$skip': page * docsPerPage },
+                          ]
+      const fileDocs = await fileColl.aggregate(filterQuery);
+      res.status(200).json(fileDocs);
     }
   }
 }
