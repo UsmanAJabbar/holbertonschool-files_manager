a300e7ac6082264be1bd3da1f63e9e3f91951bb8
scan3ls <scan3ls@gmail.com>
2021-06-09 15:55:45 -0500

    Restructure If conditions

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -11,64 +11,61 @@ class FilesController {
     const token = req.headers['x-token'];
 
     const mongoUserId = await RedisClient.get(`auth_${token}`);
-    if (!mongoUserId) res.status(401).json({ error: 'Unauthorized' })
-    else {
+    if (!mongoUserId) return res.status(401).json({ error: 'Unauthorized' });
 
-      const database = await DBClient.connection;
-      let collection = database.collection('users')
-      const user = await collection.findOne({ _id: new mongo.ObjectID(mongoUserId) });
+    const database = await DBClient.connection;
+    let collection = database.collection('users')
+    const user = await collection.findOne({ _id: new mongo.ObjectID(mongoUserId) });
 
-      const { name, type, parentId = 0, isPublic = false, data } = req.body;
-      const fileTypeOptions = ['folder', 'file' , 'image'];
+    const { name, type, data } = req.body;
+    let { parentId, isPublic } = req.body;
 
-      if (user && name && fileTypeOptions.includes(type)) {
-        const path = process.env['FOLDER_PATH'] || '/tmp/files_manager';
-        const file = {
-          userId: user._id,
-          name,
-          type,
-          isPublic,
-          parentId,
-        };
-  
-        if (['file', 'image'].includes(file.type)) {
-          if (file.parentId !== 0){
-            path = `${path}/${file.parentId}`;
-          }
-          file.localPath = `${path}/${uuid4()}`;
+    const fileTypeOptions = ['folder', 'file' , 'image'];
+
+    if (!name) return res.status(400).json({ error: 'Missing name' });
+    if (!type || !fileTypeOptions.includes(type)) return res.status(400).json({ error: 'Missing type' });
+    if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
+    
+    if (user && name && fileTypeOptions.includes(type)) {
+      let path = process.env['FOLDER_PATH'] || '/tmp/files_manager';
+      const file = {
+        userId: user._id,
+        name,
+        type,
+        isPublic,
+        parentId,
+      };
+
+      if (['file', 'image'].includes(file.type)) {
+        if (file.parentId !== 0){
+          path = `${path}/${file.parentId}`;
         }
-        collection = database.collection('files');
-        collection.insertOne(file, (err, result) => {
-          if (err) return;
-          const file = result.ops[0];
-  
-          const decode = (base64) => {
-            const buffer = Buffer.from(base64, 'base64');
-            return buffer.toString('utf-8');
-          };
-  
-          fs.mkdir(path, () => {
-            fs.writeFile(file.localPath, decode(data), (err) => {
-              if (err) res.status(500).send(`oh no\n${err.message}`);
-              else res.status(201).json({
-                id: file._id,
-                userId: file.userId,
-                name: file.name,
-                type: file.type,
-                isPublic: file.isPublic,
-                parentId: file.parentId,
-              });
+        file.localPath = `${path}/${uuid4()}`;
+      }
+      collection = database.collection('files');
+      collection.insertOne(file, (err, result) => {
+        if (err) return;
+        const file = result.ops[0];
+
+        const decode = (base64) => {
+          const buffer = Buffer.from(base64, 'base64');
+          return buffer.toString('utf-8');
+        };
+
+        fs.mkdir(path, () => {
+          fs.writeFile(file.localPath, decode(data), (err) => {
+            if (err) return res.status(500).send(`oh no\n${err.message}`);
+            return res.status(201).json({
+              id: file._id,
+              userId: file.userId,
+              name: file.name,
+              type: file.type,
+              isPublic: file.isPublic,
+              parentId: file.parentId,
             });
           });
-  
         });
-      } else if (!name) {
-        res.status(400).json({ error: 'Missing name' });
-      } else if (!type || !fileTypeOptions.includes(type)) {
-        res.status(400).json({ error: 'Missing type' });
-      } else if (!data && type !== 'folder') {
-        res.status(400).json({ error: 'Missing data' });
-      }
+      });
     }
   }
 
