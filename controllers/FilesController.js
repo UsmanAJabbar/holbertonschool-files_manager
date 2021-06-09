3d64e43a6d37deb154b6426d1553947e6be2f6af
scan3ls <scan3ls@gmail.com>
2021-06-09 12:01:59 -0500

    Update

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,27 +1,62 @@
 import DBClient from '../utils/db';
 import RedisClient from '../utils/redis';
+import { v4 as uuid4 } from 'uuid';
+
+const mongo = require('mongodb');
+const process = require('process');
 
 class FilesController {
-  static async postUpload (res, req) {
-    const token = res.headers['X-Token'];
+  static async postUpload (req, res) {
+    const token = req.headers['x-token'];
 
     const mongoUserId = await RedisClient.get(`auth_${token}`);
-    if (!mongoUserId) res.status(401).json({"error":"Unauthorized"})
+    if (!mongoUserId) res.status(401).json({ error:'Unauthorized' })
     else {
-      const user = DBClient.findOne({id: mongoUserId});
-      const { name, type, parentId = 0, isPublic = false, data } = req.params;
-      const fileTypeOptions = [ 'folder', 'file' , 'image' ];
+
+      const database = await DBClient.connection;
+      let collection = database.collection('users')
+      const user = await collection.findOne({ _id: new mongo.ObjectID(mongoUserId) });
+
+
+      const { name, type, parentId = 0, isPublic = false, data } = req.body;
+      const fileTypeOptions = ['folder', 'file' , 'image'];
 
       if (user && name && fileTypeOptions.includes(type)) {
-        
+
       } else if (!name) {
-        res.status(400).json({"error":"Missing name"});
+        res.status(400).json({ error: 'Missing name' });
       } else if (!fileTypeOptions.includes(type)) {
-        res.status(400).json({"error":"Missing type"});
+        res.status(400).json({ error: 'Missing type' });
       } else if (!data && type !== 'folder') {
-        res.status(400).json({"error":"Missing data"});
+        res.status(400).json({ error: 'Missing data' });
       }
 
+      const path = process.env['FOLDER_PATH'] || '/tmp/files_manager';
+      const file = {
+        userId: user._id,
+        name,
+        type,
+        isPublic,
+        parentId,
+        localPath: `${path}/${uuid4()}`,
+      };
+      
+      console.log(file);
+      res.status(201).json(file);
+
+      // collection = database.collection('files');
+      // collection.insert(file, (err, file) => {
+      //   res.status(201).json({
+      //     id: file._id,
+      //     userId: file.userId,
+      //     name: file.name,
+      //     type: file.type,
+      //     isPublic: file.isPublic,
+      //     parentId: file.parentId,
+      //   });
+      // });
     }
   }
 }
+
+module.exports = FilesController;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-// const FilesController = require('../controllers/FilesController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router({ mergeParams: true });
 
@@ -12,6 +12,6 @@ router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-// router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
